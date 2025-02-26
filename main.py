6e36c6e9f342379642596be86624c86a768d2e05
JetDev22 <thomas.roelig@gmail.com>
2025-02-26 20:31:10 +0100

    Done for the day

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     # Define Screen with width and height
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Set FPS
+    clock = pygame.time.Clock()
+    dt = 0
+
     # Main Loop
     while True:
         # Checks if user closed window
@@ -15,6 +19,7 @@ def main():
         screen.fill((0,0,0))
         # Update Screen
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
